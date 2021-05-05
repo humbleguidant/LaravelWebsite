27dab4e8e8d9d59e77de186791c5decadd4eead5
humbleguidant <37128838+humbleguidant@users.noreply.github.com>
2021-05-05 00:30:08 -0800

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,9 +1,17 @@
 <?php
+/**
+Author: Aubrey Nickerson
+Date: May 9th, 2020
+Program: 2014_10_12_000000_create_users_table.php
+Project: Counselling Website
+**/
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-
+// This migration creates a Users table.
+// The purpose is to create a user when a user
+// wishes to register for an appointment. 
 class CreateUsersTable extends Migration
 {
     /**
@@ -13,14 +21,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        // Create the users table
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('firstname');
-            $table->string('lastname');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->bigIncrements('id'); // ID AUTO-INCREMENT
+            $table->string('firstname'); // FIRST_NAME VARCHAR
+            $table->string('lastname'); // LAST_NAME VARCHAR
+            $table->string('email')->unique(); // EMAIL VARCHAR UNIQUE
+            $table->timestamp('email_verified_at')->nullable(); // EMAIL_VERIFIED_AT TIMESTAMP 
+            $table->string('password'); // PASSWORD VARCHAR
+            $table->rememberToken(); // REMEMBER_ME TOKEN
             $table->timestamps();
         });
     }
