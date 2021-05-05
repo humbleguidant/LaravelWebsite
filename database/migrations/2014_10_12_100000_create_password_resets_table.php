563800c5f668f54b91bd53b2bba6e23b0d8b030a
humbleguidant <37128838+humbleguidant@users.noreply.github.com>
2021-05-05 00:21:46 -0800

    Update 2014_10_12_100000_create_password_resets_table.php

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -1,9 +1,17 @@
 <?php
+/**
+Author: Aubrey Nickerson
+Date: May 9th, 2020
+Program: 2014_10_12_100000_create_password_resets_table.php
+Project: Counselling Website
+**/
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-
+// This migration creates a passwords_resets table
+// It contains the email, token, and the date that 
+// the password was reset.
 class CreatePasswordResetsTable extends Migration
 {
     /**
@@ -13,10 +21,11 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
+        // Create the database table
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+            $table->string('email')->index(); // EMAIL VARCHAR
+            $table->string('token'); // TOKEN VARCHAR
+            $table->timestamp('created_at')->nullable(); // CREATED_AT TIMESTAMP
         });
     }
 
