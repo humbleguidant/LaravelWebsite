16078673ce5a1817690f627fbeeca7ab5e6d22b2
humbleguidant <37128838+humbleguidant@users.noreply.github.com>
2021-05-05 21:34:29 -0800

    Update services.blade.php

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -3,6 +3,11 @@ Author: Aubrey Nickerson
 Date: May 1st, 2020
 Program: services.blade.php
 Project: Counselling Website
+
+This is the services page. It discusses what
+services are provided by the counselor such as
+relationship difficulties, developing self esteem,
+family problems, stress disorders, and so on. 
 -->
 
 <!DOCTYPE html>
@@ -273,18 +278,7 @@ Project: Counselling Website
           </div>
         </div>
       </div>
-      <div class="container-fluid px-0 py-5 bg-black">
-      	<div class="container">
-      		<div class="row">
-	          <div class="col-md-12">
-		
-	            <p class="mb-0" style="color: rgba(255,255,255,.5);"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-	  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
-	  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-	          </div>
-	        </div>
-      	</div>
-      </div>
+      
     </footer>
     
   
