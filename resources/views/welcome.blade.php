c1b06222a7440d99dc06abd797842e45c75c86e5
humbleguidant <37128838+humbleguidant@users.noreply.github.com>
2021-05-05 21:39:52 -0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,11 @@ Author: Aubrey Nickerson
 Date: May 15th, 2020
 Program: welcome.blade.php
 Project: Counselling Website
+
+This is the welcome page. It includes all
+the information that the user needs to know 
+about the website such as pricing, services, counselors
+biography, and so on. 
 -->
 
 <!DOCTYPE html>
@@ -299,18 +304,6 @@ Project: Counselling Website
             </div>
           </div>
         </div>
-      </div>
-      <div class="container-fluid px-0 py-5 bg-black">
-      	<div class="container">
-      		<div class="row">
-	          <div class="col-md-12">
-		
-	            <p class="mb-0" style="color: rgba(255,255,255,.5);"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-	  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
-	  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-	          </div>
-	        </div>
-      	</div>
       </div>
     </footer>
     
