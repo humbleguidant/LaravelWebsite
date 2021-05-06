da9d9e090104a85637ed67f4451e4598ca51bd37
humbleguidant <37128838+humbleguidant@users.noreply.github.com>
2021-05-05 21:11:58 -0800

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,6 +3,10 @@ Author: Aubrey Nickerson
 Date: May 1st, 2020
 Program: about.blade.php
 Project: Counselling Website
+
+This is the About page. It describes what the website is for
+and who the counselor is. It contains the menu navigation bar and 
+a few paragraphs of the counselors goals.
 -->
 
 <!DOCTYPE html>
@@ -154,18 +158,7 @@ I have experience over 15 years working with couples, individuals, and families 
           </div>
         </div>
       </div>
-      <div class="container-fluid px-0 py-5 bg-black">
-      	<div class="container">
-      		<div class="row">
-	          <div class="col-md-12">
-		
-	            <p class="mb-0" style="color: rgba(255,255,255,.5);"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-	  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
-	  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-	          </div>
-	        </div>
-      	</div>
-      </div>
+      
     </footer>
     
   
