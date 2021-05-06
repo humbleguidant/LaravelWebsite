8677db181f13ec74c9823e3065d98c07fb61aca3
humbleguidant <37128838+humbleguidant@users.noreply.github.com>
2021-05-05 19:55:05 -0800

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,7 +18,7 @@ requesting appointments.
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-            <!-- Title -->
+            <!-- Header -->
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
