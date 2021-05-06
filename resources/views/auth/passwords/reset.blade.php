c67544ff22d930d91cd50ecc200b74422c28e18b
humbleguidant <37128838+humbleguidant@users.noreply.github.com>
2021-05-05 20:33:28 -0800

    Update reset.blade.php

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,10 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
+
+<!-- 
+Author: Aubrey Nickerson
+Date: May 14th, 2020
+Program: reset.blade.php
+Project: Counselling Website
+
+This is the reset password page. When a user clicks 
+the reset password link from their email that they received,
+when requesting to reset their password, they are redirected
+to this page. 
+-->
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
+            
+            <!-- Header -->
                 <div class="card-header">{{ __('Reset Password') }}</div>
 
                 <div class="card-body">
@@ -16,9 +31,11 @@
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
+                            <!-- Email is automatically filled when clicking the link from the email -->
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
 
+                            <!-- Display an error message if email does not exist. -->
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -30,9 +47,12 @@
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
+                            <!-- Password -->
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
+
+                             <!-- An error message will occur if the password is empty or if the new password has already been used in the past. -->
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -44,11 +64,13 @@
                         <div class="form-group row">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
+                            <!-- Confirm Password -->
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
+                           <!-- Submit button -->
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
