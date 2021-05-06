385e24e5f8571d3e9570fb2db0972e55a4cbfea7
humbleguidant <37128838+humbleguidant@users.noreply.github.com>
2021-05-05 21:36:47 -0800

    Update teletherapy.blade.php

--- resources/views/teletherapy.blade.php
+++ resources/views/teletherapy.blade.php
@@ -3,6 +3,10 @@ Author: Aubrey Nickerson
 Date: May 17th, 2020
 Program: teletherapy.blade.php
 Project: Counselling Website
+
+This is the tele-therapy page.
+It discusses how clients can 
+improve the quality of their life.
 -->
 
 <!DOCTYPE html>
@@ -153,18 +157,6 @@ Project: Counselling Website
             </div>
           </div>
         </div>
-      </div>
-      <div class="container-fluid px-0 py-5 bg-black">
-      	<div class="container">
-      		<div class="row">
-	          <div class="col-md-12">
-		
-	            <p class="mb-0" style="color: rgba(255,255,255,.5);"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-	  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
-	  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-	          </div>
-	        </div>
-      	</div>
       </div>
     </footer>
     
