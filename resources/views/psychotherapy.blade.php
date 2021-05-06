97876c94712782a42dc3490c2b0ab818ecc5e59e
humbleguidant <37128838+humbleguidant@users.noreply.github.com>
2021-05-05 21:32:08 -0800

    Update psychotherapy.blade.php

--- resources/views/psychotherapy.blade.php
+++ resources/views/psychotherapy.blade.php
@@ -3,6 +3,10 @@ Author: Aubrey Nickerson
 Date: May 1st, 2020
 Program: psychotherapy.blade.php
 Project: Counselling Website
+
+This is the psychotherapy page. It discusses
+confidentiality, contacting the counselor, and 
+the details of psychotherapy.
 -->
 
 <!DOCTYPE html>
@@ -205,18 +209,6 @@ provide it for them.</p>
             </div>
           </div>
         </div>
-      </div>
-      <div class="container-fluid px-0 py-5 bg-black">
-      	<div class="container">
-      		<div class="row">
-	          <div class="col-md-12">
-		
-	            <p class="mb-0" style="color: rgba(255,255,255,.5);"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-	  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
-	  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-	          </div>
-	        </div>
-      	</div>
       </div>
     </footer>
     
