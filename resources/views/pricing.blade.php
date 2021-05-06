dc591d2369a01652676f503f4448a71d65d03824
humbleguidant <37128838+humbleguidant@users.noreply.github.com>
2021-05-05 21:26:30 -0800

    Update pricing.blade.php

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -3,6 +3,11 @@ Author: Aubrey Nickerson
 Date: May 1st, 2020
 Program: pricing.blade.php
 Project: Counselling Website
+
+This is the pricing page. It includes
+details of how much a counselling session costs.
+Each conselling session can range from 75 to 
+300 dollars. 
 -->
 
 <!DOCTYPE html>
@@ -202,18 +207,7 @@ Standing appointments can be arranged.  My fees are as follows:</p>
           </div>
         </div>
       </div>
-      <div class="container-fluid px-0 py-5 bg-black">
-      	<div class="container">
-      		<div class="row">
-	          <div class="col-md-12">
-		
-	            <p class="mb-0" style="color: rgba(255,255,255,.5);"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-	  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
-	  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-	          </div>
-	        </div>
-      	</div>
-      </div>
+      
     </footer>
     
   
