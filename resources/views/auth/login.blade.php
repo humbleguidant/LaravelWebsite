beaafd11d2379938df95d857b153898fcea0c9d0
humbleguidant <37128838+humbleguidant@users.noreply.github.com>
2021-05-05 20:05:53 -0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -72,6 +72,7 @@ they are redirected to the request appointment page.
                             </div>
                         </div>
 
+                    <!-- Login button. -->
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
