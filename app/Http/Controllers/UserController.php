06631a4fc8baf97b0b6deb1e85a9266e3410ca1a
humbleguidant <37128838+humbleguidant@users.noreply.github.com>
2021-05-05 22:07:15 -0800

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,7 +1,16 @@
 <?php
-
 namespace App\Http\Controllers;
+/*
+Author: Aubrey Nickerson
+Date: May 5th, 2020
+Program: UserController.php
+Project: Counselling Website Project
 
+This controller grabs the user data associated 
+with the user who just logged in and displays the first name
+and last name on the navigation bar. The user is then redirected
+to the request appointment page. 
+*/
 use App\User;
 use Illuminate\Http\Request;
 
@@ -9,8 +18,10 @@ class UserController extends Controller
 {
     public function index($user)
     {
+        // Select query on user table
         $user = User::find($user);
         
+        // Return the request appointment page. 
         return view('home', [
             'user' => $user,
         ]);
