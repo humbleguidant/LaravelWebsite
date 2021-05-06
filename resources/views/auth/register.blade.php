fdd5a3a2205ad3f50819afaef8ef854b83cd0e38
humbleguidant <37128838+humbleguidant@users.noreply.github.com>
2021-05-05 19:50:58 -0800

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,22 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
+
+<!-- 
+Author: Aubrey Nickerson
+Date: May 13th, 2020
+Program: register.blade.php
+Project: Counselling Website
+
+This is the create account page. A user must create an
+account and be verified before requesting an appointment. 
+This is to avoid bots posing as clients 
+requesting appointments.
+-->
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
+            <!-- Title -->
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
+                    
+                    <!-- Call the register route which will insert the data to the users table. -->
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
-
                         <div class="form-group row">
                             <label for="firstname" class="col-md-4 col-form-label text-md-right">First Name</label>
-
+                            
+                            <!-- First Name -->
                             <div class="col-md-6">
                                 <input id="firstname" type="text" class="form-control @error('firstname') is-invalid @enderror" name="firstname" value="{{ old('firstname') }}" required autocomplete="firstname" autofocus>
-
+                            
+                            <!-- Display an error message if first name is empty. -->
                                 @error('firstname')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -28,9 +45,11 @@
                         <div class="form-group row">
                             <label for="lastname" class="col-md-4 col-form-label text-md-right">Last Name</label>
 
+                            <!-- Last Name -->
                             <div class="col-md-6">
                                 <input id="lastname" type="text" class="form-control @error('lastname') is-invalid @enderror" name="lastname" value="{{ old('lastname') }}" required autocomplete="lastname" autofocus>
 
+                             <!-- Display an error message if last name is empty. -->
                                 @error('lastname')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -42,9 +61,11 @@
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
+                            <!-- Email -->
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
+                            <!-- Display an error message if email is empty or if email is already taken. -->
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -56,9 +77,11 @@
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
+                            <!-- Password -->
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
+                            <!-- Display an error message if password is empty or if passwords do not match with confrim password. -->
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -66,15 +89,18 @@
                                 @enderror
                             </div>
                         </div>
-
+                        
+                        
                         <div class="form-group row">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
+                            <!-- Confirm Password -->
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
-
+                        
+                        <!-- Submit data in fields to Users database table --> 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
